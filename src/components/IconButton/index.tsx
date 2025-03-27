8ac0d37874e87d6b83267957b05b3f574bf542f8
Amir Hossein <amirht.dev@gmail.com>
2025-03-27 17:23:21 +0330

    ⚛ Component - update IconButton size

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -20,7 +20,7 @@ const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
         {...props}
         ref={ref}
         className={twMerge(
-          'aspect-square ripple size-14 grid place-items-center rounded-full',
+          'aspect-square ripple size-10 grid place-items-center rounded-full',
           color === 'ghost' ? 'bg-white/8' : 'bg-primary text-primary-fg',
           className
         )}
