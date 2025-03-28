eab27d997a0060cf236309f468ca7630085fe9ef
Amir Hossein <amirht.dev@gmail.com>
2025-03-29 01:26:13 +0330

    ⚛ Component - create Footer

---
+++ src/components/Footer/index.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Footer from '.';
+
+const meta = {
+  component: Footer,
+  tags: ['!autodocs'],
+} satisfies Meta<typeof Footer>;
+
+export default meta;
+
+type Story<T = typeof meta> = StoryObj<T>;
+
+export const Default = {
+  name: 'Footer',
+} satisfies Story;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,29 @@
+const Footer = () => {
+  return (
+    <footer className="flex flex-col items-center text-surface-variant-fg gap-x-3 gap-y-3 mt-7">
+      <p className="text-body-3">
+        Copyright {new Date().getFullYear()} AmirHT. All Rights Reserved.
+      </p>
+
+      <p className="text-body-3 flex items-center gap-1">
+        Powered By{' '}
+        <a
+          href="https://openweathermap.org/api"
+          title="Free OpenWeather Api"
+          target="_blank"
+          rel="noopener"
+        >
+          <img
+            src="/images/openweather.png"
+            width={150}
+            height={50}
+            alt="OpenWeather"
+            loading="lazy"
+          />
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
