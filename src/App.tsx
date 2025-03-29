4f25fdfa8ddbd23efd324ecb28bb046ac13dac38
Amir Hossein <amirht.dev@gmail.com>
2025-03-29 13:22:24 +0330

    🔧 Component - add section to App and complete mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import CurrentWeatherCard from './components/CurrentWeatherCard';
+import FiveDayForecast from './components/FiveDayForecast';
+import Footer from './components/Footer';
 import Header from './components/Header';
+import HourlyForecast from './components/HourlyForecast';
+import TodayHighlights from './components/TodayHighlights';
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col min-h-dvh">
       <Header />
+      <main className="container flex-1 relative space-y-4">
+        <CurrentWeatherCard />
+        <FiveDayForecast />
+        <TodayHighlights />
+        <HourlyForecast />
+        <Footer />
+      </main>
     </div>
   );
 }
