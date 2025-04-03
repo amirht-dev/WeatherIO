7da92bd9c6588c41197397407696d2c1c7385a2e
Amir Hossein <amirht.dev@gmail.com>
2025-04-03 12:19:39 +0330

    🔧 pass entire location as prop in to search item

--- src/components/SearchBox/index.tsx
+++ src/components/SearchBox/index.tsx
@@ -55,10 +55,7 @@ const SearchBox = () => {
           <SearchList>
             {data?.map((item) => (
               <AutocompleteItem asChild key={item.id}>
-                <SearchItem
-                  title={item.label}
-                  subtitle={`subtitle of ${item.label}`}
-                />
+                <SearchItem location={item} />
               </AutocompleteItem>
             ))}
           </SearchList>
@@ -73,86 +70,3 @@ const SearchBox = () => {
 };
 
 export default SearchBox;
-// import { useQuery } from '@tanstack/react-query';
-// import { useDebounce } from '@uidotdev/usehooks';
-// import { Search } from 'lucide-react';
-// import { useState } from 'react';
-// import {
-//   Autocomplete,
-//   AutocompleteInput,
-//   AutocompleteItem,
-//   AutocompleteList,
-// } from '../Autocomplete';
-// import { Input, InputElement } from '../Input';
-// import Loading from '../Loading';
-// import SearchItem from '../SearchItem';
-// import SearchList from '../SearchList';
-
-// function wait(ms = 1000) {
-//   return new Promise((res) => setTimeout(res, ms));
-// }
-
-// const SearchBox = () => {
-//   const [term, setTerm] = useState('');
-
-//   const debouncedTerm = useDebounce(term, 500);
-
-//   const { data, isLoading } = useQuery({
-//     queryKey: ['search', debouncedTerm],
-//     queryFn: async () => {
-//       await wait();
-//       return Array.from({ length: 0 }, (_, idx) => ({
-//         id: idx,
-//         label: `Label ${idx}`,
-//       }));
-//     },
-//     enabled: !!debouncedTerm,
-//   });
-
-//   const isDebouncing = term !== debouncedTerm;
-
-//   return (
-//     <Autocomplete open={!!term && !isDebouncing} term={term} setTerm={setTerm}>
-//       <AutocompleteInput>
-//         {({ term, setTerm }) => (
-//           <Input className="w-[400px] border-none bg-surface rounded-xl max-laptop:hidden data-[state=open]:rounded-b-none">
-//             <Search className="shrink-0" />
-//             <InputElement
-//               autoComplete="off"
-//               placeholder="search city..."
-//               value={term}
-//               onChange={(e) => {
-//                 setTerm(e.target.value);
-//               }}
-//             />
-//           </Input>
-//         )}
-//       </AutocompleteInput>
-
-//       <AutocompleteList asChild>
-//         {isLoading ? (
-//           <div className="flex items-center justify-center h-[100px]">
-//             <Loading />
-//           </div>
-//         ) : data?.length ? (
-//           <SearchList>
-//             {data?.map((item) => (
-//               <AutocompleteItem asChild key={item.id}>
-//                 <SearchItem
-//                   title={item.label}
-//                   subtitle={`subtitle of ${item.label}`}
-//                 />
-//               </AutocompleteItem>
-//             ))}
-//           </SearchList>
-//         ) : (
-//           <div className="flex items-center justify-center h-[100px] text-surface-variant-fg italic">
-//             city not exists
-//           </div>
-//         )}
-//       </AutocompleteList>
-//     </Autocomplete>
-//   );
-// };
-
-// export default SearchBox;
--- src/components/SearchItem/index.tsx
+++ src/components/SearchItem/index.tsx
@@ -1,25 +1,27 @@
+import { Location } from '@/services/api';
 import { MapPin } from 'lucide-react';
 import { ComponentPropsWithoutRef } from 'react';
+import { twMerge } from 'tailwind-merge';
 import { Merge } from 'type-fest';
 
 type SearchItemProps = Merge<
   ComponentPropsWithoutRef<'li'>,
   {
-    title: string;
-    subtitle: string;
+    location: Location;
   }
 >;
 
-const SearchItem = ({ title, subtitle }: SearchItemProps) => {
+const SearchItem = ({ location, ...props }: SearchItemProps) => {
+  const { name, region, country } = location;
   return (
-    <li className="list-none">
-      <a href="#" className="flex items-center gap-4 px-4 py-2 ripple">
+    <li {...props} className={twMerge('list-none', props.className)}>
+      <button className="flex items-center gap-4 px-4 py-2 ripple w-full">
         <MapPin className="size-6 text-surface-variant-fg" />
-        <div className="flex flex-col">
-          <p>{title}</p>
-          <p className="text-surface-variant-fg">{subtitle}</p>
+        <div className="flex flex-col items-start">
+          <p>{name}</p>
+          <p className="text-surface-variant-fg">{`${region}, ${country}`}</p>
         </div>
-      </a>
+      </button>
     </li>
   );
 };
