fa0aa19780ae1c8192875e75a711441503efa098
Amir Hossein <amirht.dev@gmail.com>
2025-04-03 11:55:20 +0330

    📦 add react hot toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import App from './App.tsx';
 import './index.css';
 
@@ -10,6 +11,14 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          error: {
+            className: '!bg-bg-aqi-4 !text-bg-aqi-4-fg',
+          },
+        }}
+      />
     </QueryClientProvider>
   </StrictMode>
 );
