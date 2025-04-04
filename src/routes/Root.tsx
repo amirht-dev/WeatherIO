2fe79bb9404e20e7545578ca2b87fef06d095791
Amir Hossein <amirht.dev@gmail.com>
2025-04-04 19:14:16 +0330

    🔧 update bottom padding to enhance visibility

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -7,7 +7,7 @@ import TodayHighlights from '@/components/TodayHighlights';
 
 const RootPage = () => {
   return (
-    <div className="flex flex-col min-h-dvh">
+    <div className="flex flex-col min-h-dvh pb-4">
       <Header />
       <main className="container tablet:grid-rows-[masonry] flex-1 relative grid grid-cols-1 tablet:grid-cols-[280px_1fr] laptop:grid-cols-[360px_1fr] gap-4 laptop:gap-10">
         <CurrentWeatherCard />
